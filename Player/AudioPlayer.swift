5981d9c28a4722afda50717edfcf773f9cb4a948
Andrew <108402160+PussyDestroyerKrd@users.noreply.github.com>
2023-06-05 13:54:52 +0300

    add layout AudioPlayerVC

--- Player/AudioPlayer.swift
+++ Player/AudioPlayer.swift
@@ -11,19 +11,24 @@ class AudioPlayer: UIViewController {
     private let imageView = UIImageView()
     private let stackView = UIStackView()
     private let pauseButton = UIButton()
-    private let nextButton = UIButton()
+    private let forwardButton = UIButton()
     private let backButton = UIButton()
     private let slider = UISlider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setupImageView()
         setupSlider()
+        setupStackView()
+        setupButtons()
     }
     
     private func setupImageView() {
         view.addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .black
+        imageView.layer.cornerRadius = 14
         
         [imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
          imageView.leftAnchor.constraint(equalTo: view.leftAnchor,constant: 30),
@@ -44,6 +49,34 @@ class AudioPlayer: UIViewController {
     private func setupStackView() {
         view.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .equalCentering
         
+        [stackView.topAnchor.constraint(equalTo: slider.bottomAnchor, constant: 5),
+         stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         stackView.widthAnchor.constraint(equalToConstant: 150),
+         stackView.heightAnchor.constraint(equalToConstant: 60)].forEach{ $0.isActive = true }
+    }
+    
+    private func setupButtons() {
+        stackView.addArrangedSubview(backButton)
+        backButton.heightAnchor.constraint(equalToConstant: 58).isActive = true
+        
+        stackView.addArrangedSubview(pauseButton)
+        pauseButton.heightAnchor.constraint(equalToConstant: 58).isActive = true
+
+        stackView.addArrangedSubview(forwardButton)
+        forwardButton.heightAnchor.constraint(equalToConstant: 58).isActive = true
+
+        
+        pauseButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        forwardButton.setImage(UIImage(systemName: "forward.fill"), for: .normal)
+        backButton.setImage(UIImage(systemName: "backward.fill"), for: .normal)
+        
+        pauseButton.addTarget(self, action: #selector(asd), for: .touchUpInside)
+    }
+    
+    @objc func asd() {
+        pauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
     }
 }
--- Player/SceneDelegate.swift
+++ Player/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-        window.rootViewController = UINavigationController(rootViewController: MusicListViewController())
+        window.rootViewController = UINavigationController(rootViewController: AudioPlayer())
         self.window = window
     }
 
